843083ad322261c7d4ea770a9ae73e535fda2f31
Loy Yee Ko <koloyyee@gmail.com>
2022-10-03 14:28:37 +0800

    Add Weather component back.

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -4,6 +4,7 @@ import { NewscatcherArticleInterface } from "../../interfaces/Newscatcher.interf
 import loadingGif from "../../src/assets/loading.gif";
 import Greetings from "../Greeting";
 import NewsGrid from "../NewsGrid";
+import Weather from "../Weather";
 import "./style.css";
 
 type MainProps = {
@@ -23,8 +24,8 @@ const Main: React.FC<MainProps> = ({ loading, searchResult }) => {
             alt="fetching data"
           />
         ) : (
-          <></>
-          // <Weather />
+          // <></>
+          <Weather />
         )}
       </div>
       <NewsGrid searchResult={searchResult} />
